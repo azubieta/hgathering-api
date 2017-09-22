595139a0b8b066974d63bd3f7d822efa634bc247
Alexis López Zubieta <azubieta90@gmail.com>
2017-09-22 03:44:50 -0500

    agregando funcionalidad centrosDeAcopioCercanos

--- common/models/acopio.js
+++ common/models/acopio.js
@@ -1,5 +1,33 @@
 'use strict';
 
-module.exports = function(Acopio) {
+module.exports = function (Acopio) {
 
-};
+  Acopio.remoteMethod('centrosDeAcopioCercanos', {
+    description: 'Lista centros de acopio cercanos al posición en un radio especificado.',
+    accepts: [
+      {arg: 'loc', type: 'GeoPoint', required: true, description: 'ubicación de referencia'},
+      {arg: 'radio', type: 'number', required: true, description : 'distancia en kilometros'}],
+    returns: {arg: 'centros de acopio', type: 'array'}
+  });
+
+  Acopio.centrosDeAcopioCercanos = function (loc, radio, cb) {
+    var list = [];
+    Acopio.find({})
+      .then(function (result) {
+          for (var i = 0; i < result.length; i++) {
+            var centro = result[i];
+            if (centro.geopos) {
+              var distance = loc.distanceTo(centro.geopos, {type: 'kilometers'});
+              if (distance <= radio)
+                list.push(centro);
+            }
+          }
+
+          cb(null, list);
+        }
+      ).catch(function (error) {
+      cb(error)
+    });
+  }
+}
+;
