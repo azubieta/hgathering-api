c901815d2e7f2726ba40f020b8507ccbcc0392ed
Alexis López Zubieta <azubieta90@gmail.com>
2017-09-24 11:20:58 -0500

    adding status responce to the /api/ entry

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,6 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/api/', server.loopback.status());
   server.use(router);
 };
